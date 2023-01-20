d3a1735f9b2c5323df2942563a19e9302160f486
brahianr <brahian-restrepo@hotmail.com>
2023-01-20 15:16:41 -0500

    Se realiza ejercicio

---
+++ src/main/java/com/serenitydojo/Hampster.java
@@ -0,0 +1,23 @@
+package com.serenitydojo;
+
+public class Hampster extends Pet{
+
+    private String favoriteGame;
+
+    public Hampster(String name, int age, String favoriteGame) {
+        super(name, age);
+        this.favoriteGame = favoriteGame;
+    }
+
+    public String getFavoriteGame() {
+        return favoriteGame;
+    }
+
+    public void setFavoriteGame(String favoriteGame) {
+        this.favoriteGame = favoriteGame;
+    }
+
+    public String play(){
+        return "runs in wheel";
+    }
+}
